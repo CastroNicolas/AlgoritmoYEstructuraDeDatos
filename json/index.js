e19a5112cddb310df1d7b25750273190b4def8d8
CastroNicolas <maximonicolascastro@gmail.com>
2025-01-21 19:35:06 +0100

    fetch and async - await

--- json/index.js
+++ json/index.js
@@ -14,3 +14,24 @@ promise.then((res) => console.log(res)).catch((err) => console.log(err));
 // console.log(promise.then()); // undefined
 // console.log(promise.catch()); // undefined
 // console.log(promise.finally()); // undefined
+
+// fetch ==> Asincronismo
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// console.log(fetch("https://jsonplaceholder.typicode.com/todos/1")); // undefined
+
+// async - await
+const getTodos = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getTodos();
