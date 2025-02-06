16435fcf0aac62375dd76174455cda31dbfdf9bf
CastroNicolas <maximonicolascastro@gmail.com>
2025-02-06 23:53:30 +0100

    Linkend List

---
+++ Linked List - listas enlazadas/README.md
@@ -0,0 +1,12 @@
+Lista enlazadas son un conjutos de nodos que estan enlazados
+3 elementos
+
+- la cabeza = head A
+- el medio = nodo B y nodo C
+- el final = null
+
+tipo
+
+- lista enlazadas simple
+- lista enlazadas doble
+- lista enlazadas circular
---
+++ Linked List - listas enlazadas/index.js
@@ -0,0 +1,76 @@
+// listas enlazadas
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(value) {
+    const node = new Node(value);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+
+  delete(value) {
+    if (this.head === null) {
+      this.head = this.head.next;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        if (current.next.value === value) {
+          current.next = current.next.next;
+          break;
+        }
+        current = current.next;
+      }
+    }
+  }
+
+  show() {
+    let current = this.head;
+    while (current !== null) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+  size() {
+    let count = 0;
+    let current = this.head;
+    while (current !== null) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+  clear() {
+    this.head = null;
+  }
+}
+
+const linkedList = new LinkedList();
+linkedList.add("A");
+linkedList.add("B");
+linkedList.add("C");
+linkedList.add("D");
+
+console.log(linkedList.size());
+linkedList.show();
+
+linkedList.delete("B");
+console.log(linkedList.size());
+linkedList.show();
