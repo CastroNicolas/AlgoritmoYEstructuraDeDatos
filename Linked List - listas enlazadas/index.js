049da2645619209ea88f09f313670eb0955c35fb
CastroNicolas <maximonicolascastro@gmail.com>
2025-02-07 01:02:07 +0100

    example linkend list with html

--- Linked List - listas enlazadas/index.js
+++ Linked List - listas enlazadas/index.js
@@ -27,17 +27,20 @@ class LinkedList {
   }
 
   delete(value) {
-    if (this.head === null) {
-      this.head = this.head.next;
-    } else {
-      let current = this.head;
-      while (current.next !== null) {
-        if (current.next.value === value) {
-          current.next = current.next.next;
-          break;
-        }
-        current = current.next;
+    if (this.head === null) return; // Si la lista está vacía, no hace nada
+
+    if (this.head.value === value) {
+      this.head = this.head.next; // Si el valor a eliminar es el primero, mueve la cabeza
+      return;
+    }
+
+    let current = this.head;
+    while (current.next !== null) {
+      if (current.next.value === value) {
+        current.next = current.next.next; // Salta el nodo a eliminar
+        return;
       }
+      current = current.next;
     }
   }
 
@@ -63,14 +66,92 @@ class LinkedList {
 }
 
 const linkedList = new LinkedList();
-linkedList.add("A");
-linkedList.add("B");
-linkedList.add("C");
-linkedList.add("D");
+// linkedList.add("A");
+// linkedList.add("B");
+// linkedList.add("C");
+// linkedList.add("D");
 
-console.log(linkedList.size());
-linkedList.show();
+// console.log(linkedList.size());
+// linkedList.show();
 
-linkedList.delete("B");
-console.log(linkedList.size());
-linkedList.show();
+// linkedList.delete("B");
+// console.log(linkedList.size());
+// linkedList.show();
+
+// Ejemplo de uso de una lista enlazada
+
+const title = document.getElementById("movie");
+const image = document.getElementById("image");
+const next = document.getElementById("next");
+
+const moviesLinkedList = new LinkedList();
+moviesLinkedList.add({
+  title: "The Matrix",
+  //   image:
+  // "https://m.media-amazon.com/images/M/MV5BNzQzOTg3NzItYmY0Zi00YzJlLWEyYjUtODMyYjA5YzgzNzA5XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_UX182_CR0,0,182,268_AL_.jpg",
+});
+
+moviesLinkedList.add({
+  title: "Star Wars",
+  //   image:
+  // "https://m.media-amazon.com/images/M/MV5BNjg3MjM4NzY0N15BMl5BanBnXkFtZTgwMTMyMjE4MjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+});
+
+moviesLinkedList.add({
+  title: "Harry Potter",
+  //   image:
+  // "https://m.media-amazon.com/images/M/MV5BMTMxMTQzNjA0Nl5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_UY268_CR0,0,182,268_AL_.jpg",
+});
+
+// const movies = [
+//   {
+//     id: 1,
+//     title: "Avatar",
+//     image:
+//       "https://m.media-amazon.com/images/M/MV5BMjE4NzY3NzYwNF5BMl5BanBnXkFtZTgwMjY4OTg2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+//   },
+//   {
+//     id: 2,
+//     title: "Star Wars",
+//     image:
+//       "https://m.media-amazon.com/images/M/MV5BNjg3MjM4NzY0N15BMl5BanBnXkFtZTgwMTMyMjE4MjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+//   },
+//   {
+//     id: 3,
+//     title: "The Matrix",
+//     image:
+//       "https://m.media-amazon.com/images/M/MV5BNzQzOTg3NzItYmY0Zi00YzJlLWEyYjUtODMyYjA5YzgzNzA5XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_UX182_CR0,0,182,268_AL_.jpg",
+//   },
+//   {
+//     id: 4,
+//   },
+//   {
+//     id: 5,
+//     title: "The Dark Knight",
+//     image:
+//       "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+//   },
+// ];
+
+const showMovies = () => {
+  const movie = moviesLinkedList.head.value;
+  title.innerText = `Movie: ${movie.title}`;
+  //   image.src = movie.image || "NOT PICTIURED";
+
+  if (moviesLinkedList.head.next !== null) {
+    const movieNext = moviesLinkedList.head.next.value;
+    next.innerHTML = `Next Movie: ${movieNext.title}`;
+  } else {
+    next.innerHTML = "No hay peliculas";
+  }
+};
+
+const nextMovie = () => {
+  // if (moviesLinkedList.head.next !== null) }
+  if (moviesLinkedList.head.next) {
+    moviesLinkedList.delete(moviesLinkedList.head.value);
+    showMovies();
+  }
+};
+
+showMovies();
